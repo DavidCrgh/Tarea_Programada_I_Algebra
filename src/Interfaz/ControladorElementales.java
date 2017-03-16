fca474961143dc8555f0ff902726d7f84f5169d3
DavidCRgh <dav.val.gar-77@hotmail.com>
2017-03-15 18:25:47 -0600

    Ahora se abre una nueva ventana cuando se hace click en operaciones elementales.

---
+++ src/Interfaz/ControladorElementales.java
@@ -0,0 +1,22 @@
+package Interfaz;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Creado por David Valverde Garro - 2016034774
+ * Fecha: 15-Mar-17 Tiempo: 6:09 PM
+ */
+public class ControladorElementales implements Initializable{
+    //Controles
+
+    //Otras Variables
+
+    public void initialize(URL fxmlFileLocation, ResourceBundle resources){
+
+    }
+
+    //Metodos que dan funcionalidad a los controles de la ventana
+}
--- src/Interfaz/ControladorInicio.java
+++ src/Interfaz/ControladorInicio.java
@@ -1,9 +1,14 @@
 package Interfaz;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -11,20 +16,38 @@ import java.util.ResourceBundle;
  * Creado por David Valverde Garro - 2016034774
  * el 10-Mar-17.
  */
-public class ControladorInicio implements Initializable{
+public class ControladorInicio implements Initializable{                                                                 //los controladores deben implementar Initializable
+    //Controles
     @FXML
     public Button botonOpsElementales;
     @FXML
     public Button ecuacionesMatriciales;
 
+    //Otras variables
+
+    //Metodo inicializador de la ventana                                                                                 Aqui va el codigo que asigna los atributos de los controles
     public void initialize(URL fxmlFileLocation, ResourceBundle resources){
        botonOpsElementales.setOnAction(event -> {
            abrirVentanaElementales();
        });
     }
 
-    //Metodos que dan funcionalidad a los controles de la ventana
-    public void abrirVentanaElementales(){
+    //Metodos de Funcionalidad                                                                                           Dan fancionalidad a botones, cajas de texto, etc.
+    public void abrirVentanaElementales(){                                                                               //Ejemplo de como abrir una nueva ventana desde otra
+        Stage escenario = new Stage();
+        FXMLLoader loader = new FXMLLoader();
+        Parent raiz = null;                                                                                              //Se crean estos tres objetos
+        try {
+            raiz = loader.load(getClass().getResource("Elementales.fxml").openStream());                           //Con esto se indica el FXML de la nueva ventana
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ControladorElementales controlador = (ControladorElementales) loader.getController();                            //Se instancia el controlador respectivo, se debe hacer un casting para que funcione.
+        escenario.setTitle("");
+        escenario.setScene(new Scene(raiz));
+        escenario.show();
 
+        Stage temporal = (Stage) botonOpsElementales.getScene().getWindow();                                             //Se obtiene el stage al que pertenece el boton que abre la nueva ventana para poder cerrarla
+        temporal.close();
     }
 }
