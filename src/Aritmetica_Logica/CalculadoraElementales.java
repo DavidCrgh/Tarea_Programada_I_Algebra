f0b36885bc7ffade60258417ddee654a11f701cd
David Valverde <dav.val.gar-77@hotmail.com>
2017-03-17 10:01:21 -0600

    Agregadas las operaciones elementales en Aritmetica_Logica.CalculadoraElementales

--- src/Aritmetica_Logica/CalculadoraElementales.java
+++ src/Aritmetica_Logica/CalculadoraElementales.java
@@ -24,5 +24,20 @@ public class CalculadoraElementales {
          }
     }
 
-    //public void
+    public void sumarFilas(ArrayList<ArrayList<Fraccion>> matriz, int fila1, int fila2, Fraccion escalar){
+        ArrayList<Fraccion> filaPorEscalar = new ArrayList<>(matriz.get(fila2));
+
+        for(int i = 0; i < filaPorEscalar.size(); i++){
+            filaPorEscalar.set(i,Fraccion.multiplicar(filaPorEscalar.get(i),escalar));
+        }
+
+        Fraccion operando1Actual;
+        Fraccion operando2Actual;
+        for(int j = 0; j < matriz.get(fila1).size(); j++){
+            operando1Actual = matriz.get(fila1).get(j);
+            operando2Actual = filaPorEscalar.get(j);
+
+            matriz.get(fila1).set(j,Fraccion.sumar(operando1Actual,operando2Actual));
+        }
+    }
 }
