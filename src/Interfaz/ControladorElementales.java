7326a3ccff83cb4b4a6090976ecee49f83175336
DavidCRgh <dav.val.gar-77@hotmail.com>
2017-03-18 17:54:54 -0600

    Creada la interfaz de la calculadora de elementales.

--- src/Interfaz/ControladorElementales.java
+++ src/Interfaz/ControladorElementales.java
@@ -1,6 +1,9 @@
 package Interfaz;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -11,6 +14,48 @@ import java.util.ResourceBundle;
  */
 public class ControladorElementales implements Initializable{
     //Controles
+    @FXML
+    public GridPane matrizActual;
+    @FXML
+    public GridPane matrizHistorica;
+    @FXML
+    public ComboBox<String> seleccionadorFila1;
+    @FXML
+    public ComboBox<String> seleccionadorOperacion;
+    @FXML
+    public TextField entradaEscalar;
+    @FXML
+    public ComboBox<String> seleccionadorFila2;
+    @FXML
+    public Button botonResolver;
+    @FXML
+    public Button botonAnterior;
+    @FXML
+    public Label indicador;
+    @FXML
+    public Button botonSiguiente;
+    @FXML
+    public MenuItem filas1;
+    @FXML
+    public MenuItem filas2;
+    @FXML
+    public MenuItem filas3;
+    @FXML
+    public MenuItem filas4;
+    @FXML
+    public MenuItem filas5;
+    @FXML
+    public MenuItem columnas1;
+    @FXML
+    public MenuItem columnas2;
+    @FXML
+    public MenuItem columnas3;
+    @FXML
+    public MenuItem columnas4;
+    @FXML
+    public MenuItem columnas5;
+    @FXML
+    public MenuItem reset;
 
     //Otras Variables
 
