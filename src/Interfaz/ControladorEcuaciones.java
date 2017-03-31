748173e2c5b12effb87f32be58d37d1af3576f5a
DavidCRgh <dav.val.gar-77@hotmail.com>
2017-03-30 22:24:13 -0600

    Creada la interfaz de la segunda parte.

---
+++ src/Interfaz/ControladorEcuaciones.java
@@ -0,0 +1,21 @@
+package Interfaz;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.GridPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Creado por David Valverde Garro - 2016034774
+ * Fecha: 30-Mar-17 Tiempo: 10:07 PM
+ */
+public class ControladorEcuaciones implements Initializable {
+    @FXML
+    public GridPane matrizA;
+
+    public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
+
+    }
+}
